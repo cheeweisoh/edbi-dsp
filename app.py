c9e77d3cf9eb775d87c18717c9ed99ef310cd792
cheewei <sohcheewei@u.nus.edu>
2026-02-06 10:23:50 +0800

    updated main layout to include query ui

--- app.py
+++ app.py
@@ -2,12 +2,26 @@ import streamlit as st
 from ui.layout import render_header
 from ui.tabs import render_tabs
 from ui.pages import render_dataset_page
+from ui.query import render_query_data
+from services.text2sql_service import load_model
+
+if "model" not in st.session_state:
+    st.session_state["model"] = load_model()
+    print("model loaded")
+
+model = st.session_state["model"]
 
 st.set_page_config(page_title="Data Sharing Platform", layout="wide")
 
 render_header()
 
-if "selected_dataset" in st.session_state:
-    render_dataset_page()
-else:
-    render_tabs()
+top_tabs = st.tabs(["Query Data", "All Products"])
+
+with top_tabs[0]:
+    if "selected_dataset" in st.session_state:
+        render_dataset_page()
+    else:
+        render_tabs()
+
+with top_tabs[1]:
+    render_query_data(model)
